61165800cb2887d01fa31ff98d273a1f079484ce
ilker karatas <karatasilker9@gmail.com>
2023-03-10 14:59:40 +0300

    DataAccesLayer düzeltmesi

---
+++ DataAccesLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Abstract
+{
+    public interface IGenericDAL<Entity> where Entity:class
+    {
+        void Add(Entity entity);
+        void Update(Entity entity);
+        void Delete(Expression<Func<Entity, bool>> obj);
+        Entity GetByFilter(Expression<Func<Entity, bool>> obj);
+        List<Entity> GetList(Expression<Func<Entity, bool>> obj = null);
+        void Save();
+    }
+}
